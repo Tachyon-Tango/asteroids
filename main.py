ae88e2a35603f09a0bcc77a95ab2e067197d284e
Tachyon Tango <TachyonTango@pm.me>
2025-02-25 23:28:20 -0800

    Added the game clock to the main game loop.

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while(True): # Primary game loop
         for event in pygame.event.get():
@@ -19,8 +21,12 @@ def main():
 
         screen.fill("black")
 
-        # Be sure to call this last function last as it refreshes the screen.
+        # Refreshes the screen
         pygame.display.flip()
+
+        # Increments game time
+        dt_ms = clock.tick(60)
+        dt = dt_ms/1000
 
 
 if __name__ == "__main__":
